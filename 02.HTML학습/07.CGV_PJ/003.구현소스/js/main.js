6709fde5a7a7776f0137813ec68f3741cb56c030
Tom B. Parker <tombap8@hotmail.com>
2022-10-07 11:21:03 +0900

    ss

--- 02.HTML학습/07.CGV_PJ/003.구현소스/js/main.js
+++ 02.HTML학습/07.CGV_PJ/003.구현소스/js/main.js
@@ -1,68 +1,82 @@
 // CGV PJ 메인 페이지 JS - main.js
 
-// 예고편 선택메뉴 a요소 -> movlink변수
-var movlink = document.querySelectorAll(".mlist a");
-console.log(movlink);
-// html요소를 변수에 담으면
-// 여러개의 요소일 경우 querySelectorAll()을 사용하여
-// HTML컬렉션에 저장함!
-// 여러개의 내부 주소가 있고 번호로 되어있음
-// 0부터 시작함!
-// 순번의 요소를 선택하는 방법은?
-// 컬렉션변수.item(순번) 또는 컬렉션변수[순번]
+//////////////////// 로드구역 ///////////////////
+window.addEventListener("DOMContentLoaded", () => {
 
-// a요소 개수
-var len = movlink.length;
-// length는 개수를 구해오는 속성
+    // 호출확인!
+    console.log("로딩완료!");
 
-// for문을 이용하여 링크 설정하기
-// for(시;한;증){실행코드}
+    // 예고편 선택메뉴 a요소 -> movlink변수
+    const movlink =
+        document.querySelectorAll(".mlist a");
+    // console.log(movlink);
+    // html요소를 변수에 담으면
+    // 여러개의 요소일 경우 querySelectorAll()을 사용하여
+    // HTML컬렉션에 저장함!
+    // 여러개의 내부 주소가 있고 번호로 되어있음
+    // 0부터 시작함!
+    // 순번의 요소를 선택하는 방법은?
+    // 컬렉션변수.item(순번) 또는 컬렉션변수[순번]
 
-for (var i = 0; i < len; i++) {
+    // a요소 개수
+    const len = movlink.length;
+    // length는 개수를 구해오는 속성
 
-    // 순서대로 a요소에 click 이벤트 설정하기!
-    // 대상: movlink변수
-    movlink.item(i).onclick = function(){chgMV(this)};
-    // onclick은 a요소의 이벤트 속성임
-    // 이퀄 오른쪽에 할당되는데
-    // 이때 chgMV() 함수를 직접쓰면 함수가 바로 실행됨
-    // 따라서 이벤트 발생시 호출하려면
-    // 익명함수 즉, function(){} 안에 써야함!
-    // 이것은 마치 <a href="" onclick="chgMV()">
-    // 라고 설정한것과 같다!!!
+    // for문을 이용하여 링크 설정하기
+    // for(시;한;증){실행코드}
 
-    // 이벤트에 할당한 익명함수 안의 함수에 this를 보내면
-    // 이벤트가 걸린 요소자신이 함수에 전달된다!!!
+    for (let i = 0; i < len; i++) {
 
-    // console.log("진짜도니?", i);
+        // 순서대로 a요소에 click 이벤트 설정하기!
+        // 대상: movlink변수
+        movlink.item(i).onclick = function () {
+            chgMV(this)
+        };
+        // onclick은 a요소의 이벤트 속성임
+        // 이퀄 오른쪽에 할당되는데
+        // 이때 chgMV() 함수를 직접쓰면 함수가 바로 실행됨
+        // 따라서 이벤트 발생시 호출하려면
+        // 익명함수 즉, function(){} 안에 써야함!
+        // 이것은 마치 <a href="" onclick="chgMV()">
+        // 라고 설정한것과 같다!!!
+
+        // 이벤트에 할당한 익명함수 안의 함수에 this를 보내면
+        // 이벤트가 걸린 요소자신이 함수에 전달된다!!!
+
+        // console.log("진짜도니?", i);
 
 
-} ////////// for /////////////
+    } ////////// for /////////////
+}); ////////////// 로드구역 /////////////////////
+
 
 /***************************************** 
     함수명: chgMV
     기능: iframe의 영화예고편 변경하기!
 *****************************************/
-function chgMV(ele){ // ele -> 전달되는 a요소
+function chgMV(ele) { // ele -> 전달되는 a요소
     // 전달변수는 선언없이 바로 씀!
 
     // 0. 전달되는 a요소의 "data-mv" 속성값 읽어오기
-    var minfo = ele.getAttribute("data-mv");
+    let minfo = ele.getAttribute("data-mv");
     // getAttribute(속성명) -> 요소의 속성값 읽어오는 내장함수
     // setAttribute(속성명,값) -> 요소값셋팅 내장함수
 
     // 1. 호출확인
-    console.log("바꿔!",minfo);
+    // console.log("바꿔!",minfo);
 
     // 2. 변경대상: #screen iframe
-    var tg = document.querySelector("#screen iframe");
+    const tg =
+        document.querySelector("#screen iframe");
 
     // 3. 변경내용: 대상요소의 src속성을 변경함!
     // src값 중 동영상 ID만 변수에 담긴것으로 변경하여 반영함!
-    tg.src = 
-    `https://www.youtube.com/embed/${minfo}?autoplay=1`;
+    tg.src =
+        `https://www.youtube.com/embed/${minfo}?autoplay=1`;
 
 } ////////////// chgMV 함수 ////////////////
+
+
 
 
 
@@ -77,12 +91,11 @@ function chgMV(ele){ // ele -> 전달되는 a요소
 // 1. 변수에 할당하는 방법 -> 변수명이 함수명이 됨!
 // 2. 이벤트와 연결하는 방법 -> 이벤트가 발생시 함수실행
 
-console.log("난 바깥이야!");
+// console.log("난 바깥이야!");
 
-var myFn = function(){
+var myFn = function () {
     console.log("난 안이야!");
 }
 
 // 익명함수 호출!
-myFn();
-
+// myFn();
