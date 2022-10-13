4f8f396f9a2d7ecbda37220f35428d52935b7bee
Tom B. Parker <tombap8@hotmail.com>
2022-10-13 10:58:10 +0900

    ss

--- 03.CSS학습/04.3DAni/js/iphone.js
+++ 03.CSS학습/04.3DAni/js/iphone.js
@@ -9,7 +9,19 @@ window.addEventListener("DOMContentLoaded", () => {
         2. 이 영역이 이벤트 대상이 됨
         3. 오버시 변경대상인 아이폰에 회전값 변경
         4. 트랜지션으로 애니메이션 설정적용!
+        5. 애니메이션 후에 이벤트박스를 보이게함
+        (최초 width:0 -> width:100vw)
     *************************************/
+    // 0. 애니메이션 후에 이벤트 박스 보이게함!
+    // 일정시간후 실행함수
+    // setTimeout(함수,시간)
+    // 함수는 실행코드
+    // 시간은 1/1000초(단위안씀->1000은 1초)
+    setTimeout(()=>{
+        document.querySelector(".evt")
+        .style.width = "100vw";
+    },5000); // 5초후 코드실행함! ///
+
   // 1. 대상선정
   // (1) 이벤트 대상: .evt div -> 여러개!!!
   const evt = document.querySelectorAll(".evt div");
@@ -26,8 +38,15 @@ window.addEventListener("DOMContentLoaded", () => {
         // console.log(ele,"/",idx);
         // 각 요소에 마우스오버 셋팅
         ele.onmouseover = ()=>{
-            console.log(
-                "나의 순번:",idx,setdeg[idx]);
+            // console.log(
+            //     "나의 순번:",idx,setdeg[idx]);
+
+            // 아이폰에 회전값 넣기!
+            iphone.style.transform =
+            `rotateX(10deg) rotateY(${setdeg[idx]}deg)`;
+            // 아이폰에 트랜지션 넣기!
+            iphone.style.transition = ".4s ease-out";
+
         }; /////// mouseover ////
 
     }); /////////// forEach ///////////
