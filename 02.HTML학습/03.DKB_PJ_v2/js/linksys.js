fc914571160ce85e07f43c1a548988f37ae90237
Tom B. Parker <tombap8@hotmail.com>
2022-10-14 15:29:25 +0900

    ss

---
+++ 02.HTML학습/03.DKB_PJ_v2/js/linksys.js
@@ -0,0 +1,11 @@
+// 도깨비 PJ v.2 링크시스템 JS - linksys.js
+
+////////////// 로드구역 ///////////////////
+window.addEventListener("DOMContentLoaded",()=>{
+
+    console.log("로딩완료!");
+
+    /// 링크 대상: .gnb a
+    
+
+});/////////// 로드구역 ///////////////////////
