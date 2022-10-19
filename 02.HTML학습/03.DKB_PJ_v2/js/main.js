23d10bae5e7b0981af3f60f1308d91585c0cc2e9
Tom B. Parker <tombap8@hotmail.com>
2022-10-19 11:00:06 +0900

    ss

---
+++ 02.HTML학습/03.DKB_PJ_v2/js/main.js
@@ -0,0 +1,16 @@
+// 도깨비 PJ v2 - 메인 페이지 JS - main.js
+
+////////////////// 로드구역 ////////////////
+window.addEventListener("DOMContentLoaded",
+()=>{
+    console.log("로딩완료!");
+
+    // 햄버거버튼 클릭시 .top에 .on추가/제거
+    // 전체메뉴 보이기 디자인 구현이 이미되어있음!
+    // 대상: .ham, .top
+    const ham = document.querySelector(".ham");
+    const top = document.querySelector(".top");
+
+    ham.onclick = () => top.classList.toggle("on");
+
+}); ///////////// 로드구역 /////////////////
