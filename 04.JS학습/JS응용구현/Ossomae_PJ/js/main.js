81d336cd7b2bce09235494534d8edeb5946bb5ca
Tom B. Parker <tombap8@hotmail.com>
2022-10-25 12:20:14 +0900

    Update main.js

--- 04.JS학습/JS응용구현/Ossomae_PJ/js/main.js
+++ 04.JS학습/JS응용구현/Ossomae_PJ/js/main.js
@@ -15,8 +15,18 @@ window.addEventListener("DOMContentLoaded", () => {
     // 이벤트 대상: .abtn
     const abtn = document.querySelectorAll(".abtn");
     // 0번째는 왼쪽버튼, 1번째는 오른쪽버튼
-    abtn[0].onclick = () => goSlide(0);
-    abtn[1].onclick = () => goSlide(1);
+    abtn[0].onclick = () => {
+        // 인터발지움 함수 호출!
+        clearAuto();
+        // 슬라이드 이동함수 호출!
+        goSlide(0);
+    };
+    abtn[1].onclick = () => {
+        // 인터발지움 함수 호출!
+        clearAuto();
+        // 슬라이드 이동함수 호출!
+        goSlide(1);
+    };
 
     // 광클금지 상태변수
     let prot = 0; // 0-허용,1-불허용
@@ -59,24 +69,31 @@ window.addEventListener("DOMContentLoaded", () => {
         } /////////// else /////////
     } //////////// goSlide 함수 //////////
 
-
     // 인터발용변수
     let autoI;
 
     // 자동넘기기 /////
-    // 인터발함수를 지우려면 변수에 넣고 
+    // 인터발함수를 지우려면 변수에 넣고
     // clearInterval(변수) 해야함!!!
-    autoI = setInterval(() => {
-        goSlide(1);
-    }, 2000);
+    function slideAuto() {
+        autoI = setInterval(() => {
+            goSlide(1);
+        }, 2000);
+    } //////// slideAuto 함수 //////////
 
-    // 인터발 지우기 함수
+    // 인터발함수 최초호출!
+    slideAuto();
+    // 타임아웃용 변수
+    let autoT;
+
+    // 인터발 지우기 함수 ///////////
     function clearAuto() {
         console.log("인터발지워!!!");
+        // 1. 인터발 지우기
         clearInterval(autoI);
+        // 2. 타임아웃 지우기(실행쓰나미 방지!)
+        clearTimeout(autoT);
+        // 3. 일정시간 후 다시 인터발 호출!
+        autoT = setTimeout(slideAuto, 5000);
     } /////// clearAuto함수 ////////
-
-
-
-
 }); ///////////// 로드구역 ///////////////////
