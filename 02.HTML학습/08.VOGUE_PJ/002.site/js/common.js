c58a722d19d2aab8bb1a8e56970e027dc5d18a8d
Tom B. Parker <tombap8@hotmail.com>
2022-10-31 14:42:49 +0900

    Update common.js

--- 02.HTML학습/08.VOGUE_PJ/002.site/js/common.js
+++ 02.HTML학습/08.VOGUE_PJ/002.site/js/common.js
@@ -11,26 +11,43 @@ $(() => {
     ////// 스크롤 이벤트 구역 /////////////
     // 이벤트명 : scroll
     // 이벤트 대상: window
+    // 변경대상: #top
+    const topA = $("#top");
     // 스크롤위치변수
     let scTop;
+    // 마지막 스크룰위치값
+    let lastSc = 0;
 
     $(window).scroll(() => {
         // 스크롤 위치값(this는 window)
         scTop = $(this).scrollTop();
         // scrollTop() 메서드
         // - 세로스크롤 위치값을 리턴하는 메서드
-        console.log(scTop);
+        // console.log(scTop);
 
         // 1. 슬림메뉴 클래스on적용
         // 기준위치는 스크롤위치 100px이상
         if(scTop >= 100){ // 100px이상
-            $().addClass("on");
+            topA.addClass("on");
             // addClass(클래스명) - 클래스넣기
         } ///////// if /////////
         else{ // 100px 미만
-            $().removeClass("on");
+            topA.removeClass("on");
             // removeClass(클래스명) - 클래스지우기
         } //////// else /////////
+
+        //////////////////////////
+        // 스크롤 방향 알아내기 ///
+        if(scTop > lastSc){
+            console.log("아랫방향!");
+        }
+        else{
+            console.log("윗방향!");
+        }
+        // 마지막위치 업데이트 필수!
+        lastSc = scTop;
+
+
 
     }); //////// scroll /////////////////
 }); //////////////// jQB ///////////////////
