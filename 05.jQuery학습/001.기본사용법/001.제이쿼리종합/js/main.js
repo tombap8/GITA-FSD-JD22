f80f81e6b702f59c93f0d8611555cca961d98f77
Tom B. Parker <tombap8@hotmail.com>
2022-11-14 11:16:00 +0900

    Update main.js

--- 05.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
+++ 05.jQuery학습/001.기본사용법/001.제이쿼리종합/js/main.js
@@ -1,5 +1,3 @@
-const { func } = require("prop-types");
-
 // 미니언즈 좀비 탈출 애니 구현 JS - main.js
 $(() => {
     /////////// jQB ///////////////////
@@ -415,8 +413,25 @@ $(() => {
                         "linear", // 등속도
                         () => {
                             // 최종마무리구역
+
+                            // 간판 떨어뜨리기
+                            // 1단계 : 중간까지 떨어짐
+                            // -> 간판에 class "on"주기
+                            let tit = $(".tit");
+                            tit.addClass("on");
+                            setTimeout(() => {
+                                // -> 간판에 class "on2"주기
+                                ()=>tit.addClass("on2")
+                            }, 3000);
+
+                            // 빌딩 무너뜨리기
+                            // 간판 떨어진 후 실행(6초)
+                            setTimeout(() => {
+                                bd.parent().addClass("on")
+                                // parent() 부모요소인 .building
+                            }, 6000);
                         }
-                    );
+                    ); ///////// animate /////////////
             };
 
             // 액션함수호출
