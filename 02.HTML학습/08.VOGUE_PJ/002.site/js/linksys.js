ce6948cbf5a887e0a4eb607b13232a09d7f4ea8f
Tom B. Parker <tombap8@hotmail.com>
2022-11-14 16:20:03 +0900

    ss

--- 02.HTML학습/08.VOGUE_PJ/002.site/js/linksys.js
+++ 02.HTML학습/08.VOGUE_PJ/002.site/js/linksys.js
@@ -6,6 +6,36 @@ $(() => {
     // 로딩확인
     console.log("로딩완료!");
 
+    /**************************************** 
+        로그인, 회원가입, 갤러리 아이콘 넣기
+    ****************************************/
+    // 대상: .sns a:last-child (마지막 카스링크)
+    // 변경내용: 대상요소 앞에 형제요소로 a요소 삽입
+    // 메서드: before(요소) -> 선택요소 앞에 형제요소로 추가
+    // -> 참고) after(요소) -> 선택요소 뒤에 형제요소로 추가
+    // 선택자 :last (제이쿼리전용!)
+    $(".sns a:last").before(`
+        <a href="#" class="fi fi-laptop">
+            <span class="ir"> 로그인 </span>
+        </a>
+        <a href="#" class="fi fi-user-secret">
+            <span class="ir"> 회원가입 </span>
+        </a>
+        <a href="#" class="fi fi-camera">
+            <span class="ir"> 갤러리 </span>
+        </a>
+   `);
+
+   ///// sns 파트 a 요소들에 툴팁넣기 //////
+   // each((순번,요소)=>{구현부})
+   $(".sns a").each((idx,ele)=>{
+        // attr(속성명,값)
+        // -> 값으로 자식요소인 .ir의 텍스트읽어감!
+        $(ele).attr("title",$(ele).children(".ir").text().trim());
+        // trim() 앞뒤공백제거
+
+   }); ///////// each ////////////
+
     /********************************** 
         GNB 메뉴 파트 링크 셋팅하기
     **********************************/
@@ -24,8 +54,7 @@ $(() => {
         // 2. 서브페이지 이동하기
         if (atxt !== "search") {
             // 검색이 아니면!
-            location.href = "category.html?cat=" + 
-            encodeURIComponent(atxt);
+            location.href = "category.html?cat=" + encodeURIComponent(atxt);
             // 카테고리를 구분하기 위한 파라미터 키=값 쌍을 보냄
             // cat=카테고리명
             // 이것을 받아서 페이지 셋업을 한다!
---
+++ 02.HTML학습/08.VOGUE_PJ/002.site/js/member.js
@@ -0,0 +1,1 @@
+// 보그 PJ 회원가입 페이지 JS - member.js
