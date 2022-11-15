95644ffa1e0b76330eaafb70589790491aa666ad
Tom B. Parker <tombap8@hotmail.com>
2022-11-15 12:02:00 +0900

    Update 03.JSX.jsx

--- 06.React학습/js/03.JSX.jsx
+++ 06.React학습/js/03.JSX.jsx
@@ -1,4 +1,4 @@
-// 03.JSX.js 
+// 03.JSX.js
 
 /******************************************************* 
     [ JSX란 무엇인가? ]
@@ -13,8 +13,7 @@
 // 넣을 요소
 const myele1 = <h1>나는 JSX를 사용하고 있어!</h1>;
 // 리액트 루트생성하기 : createRoot() 메서드 사용!
-const root1 =  ReactDOM.createRoot(
-    document.querySelectorAll("#root div")[0]);
+const root1 = ReactDOM.createRoot(document.querySelectorAll("#root div")[0]);
 // 적용하기 : 생성된 루트에 render()메서드를 붙여서 쓸 수 있다!
 root1.render(myele1);
 
@@ -31,13 +30,11 @@ root1.render(myele1);
 // 2. JSX 를 사용하지 않한 예 //////////////////
 
 // 넣을 요소를 createElement()메서드로 생성함(JSX쓰지않고...)
-const myele2 = 
-React.createElement("h1",{},"나는 JSX를 쓰지 않아!");
+const myele2 = React.createElement("h1", {}, "나는 JSX를 쓰지 않아!");
 // createElement(요소명,{JS코드작성},요소내용)
 
 // 리액트 루트생성하기 : createRoot() 메서드 사용!
-const root2 = ReactDOM.createRoot(
-    document.querySelectorAll("#root div")[1]);
+const root2 = ReactDOM.createRoot(document.querySelectorAll("#root div")[1]);
 // 적용하기 : 생성된 루트에 render()메서드를 붙여서 쓸 수 있다!
 root2.render(myele2);
 
@@ -62,9 +59,8 @@ let num1 = 1000;
 let num2 = 7;
 
 // 3. JSX 표현식 사용하기
-const myele3 = <h1>리액트는 {num1*num2} 번 사용해도 좋다!</h1>;
-const root3 = ReactDOM.createRoot(
-    document.querySelectorAll("#root div")[2]);
+const myele3 = <h1>리액트는 {num1 * num2} 번 사용해도 좋다!</h1>;
+const root3 = ReactDOM.createRoot(document.querySelectorAll("#root div")[2]);
 root3.render(myele3);
 
 /*********************************************************** 
@@ -95,9 +91,7 @@ const myele4 = (
         </ul>
     </React.Fragment>
 );
-const root4 = ReactDOM.createRoot(
-    document.querySelectorAll("#root div")[3]
-);
+const root4 = ReactDOM.createRoot(document.querySelectorAll("#root div")[3]);
 root4.render(myele4);
 
 //// 5번에는 내가 원하는 태그요소를 출력해 본다!
@@ -113,9 +107,7 @@ const myele5 = (
         </ul>
     </React.Fragment>
 );
-const root5 = ReactDOM.createRoot(
-    document.querySelectorAll("#root div")[4]
-);
+const root5 = ReactDOM.createRoot(document.querySelectorAll("#root div")[4]);
 root5.render(myele5);
 
 /***************************************************** 
@@ -123,11 +115,8 @@ root5.render(myele5);
     예) <br> -> <br />
         <input type="text"> -> <input type="text" />
 *****************************************************/
-const myele6 = 
-<input type="text" value="홀로태그는 스스로 닫아라!" />;
-const root6 = ReactDOM.createRoot(
-    document.querySelectorAll("#root div")[5]
-);
+const myele6 = <input type="text" value="홀로태그는 스스로 닫아라!" />;
+const root6 = ReactDOM.createRoot(document.querySelectorAll("#root div")[5]);
 root6.render(myele6);
 /**************************************************** 
     [ JSX에서 속성클래스는 className 으로 표기한다! ]
@@ -135,9 +124,40 @@ root6.render(myele6);
     class는 JS에서 키워드이므로 사용못함! 대신...
     <태그 className="클래스명">
 ****************************************************/
-const myele7 = 
-<h1 className="myclass">className속성으로 클래스셋팅!!!</h1>;
-const root7 = ReactDOM.createRoot(
-    document.querySelectorAll("#root div")[6]
-);
+const myele7 = <h1 className="myclass">className속성으로 클래스셋팅!!!</h1>;
+const root7 = ReactDOM.createRoot(document.querySelectorAll("#root div")[6]);
 root7.render(myele7);
+
+/********************************************************** 
+    [ JSX에서 조건문 사용하기 - if문 ]
+    리액트는 if명령문을 지원하지만
+    JSX내부에서는 지원하지 않는다!
+
+    JSX에서 조건문을 사용하려면?
+    JSX 외부에서 if문을 사용하거나
+    아니면 내부에서 삼항연산자를 사용할 수 있다!
+**********************************************************/
+// JSX외부에서 if문 사용하기
+const x = 1000;
+let txt = "이 돈으로는 충분히 살 수 있어!";
+if (x < 10000) {
+    txt = "돈이 부족해서 살 수 없어!";
+} /////// if ///////
+
+const myele8 = <h1>{txt}</h1>;
+const root8 = ReactDOM.createRoot(document.querySelectorAll("#root div")[7]);
+root8.render(myele8);
+
+// JSX 표현식에 삼항연산자 사용하기
+
+let time = 8;
+
+const myele9 =( 
+    <React.Fragment>
+        <h1>지금 몇시지? {time}시야!</h1>
+        <h1>{time > 9 ? "지금 집에 들어와!" : "더 놀다와~!"}</h1>
+    </React.Fragment>
+);
+const root9 = ReactDOM.createRoot(
+    document.querySelectorAll("#root div")[8]);
+root9.render(myele9);
