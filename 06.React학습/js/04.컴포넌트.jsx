615a8c4629c188c68db75c8c9dd83bd0500db380
Tom B. Parker <tombap8@hotmail.com>
2022-11-16 11:18:02 +0900

    Update 04.컴포넌트.jsx

--- 06.React학습/js/04.컴포넌트.jsx
+++ 06.React학습/js/04.컴포넌트.jsx
@@ -32,7 +32,7 @@
 
 *************************************************/
 
-// 클래스로 컴포넌트 작성
+// [ 클래스로 컴포넌트 작성 ] /////////
 class Gogh extends React.Component {
     // render() 메서드 사용함!
     render(){
@@ -56,3 +56,44 @@ root1.render(<Gogh />);
 // render(출력할 요소)
 // 출력할 요소 -> 클래스를 호출하면 구성된 태그가 들어옴
 // 클래스 호출법: <클래스명 />
+
+// [ 함수로 컴포넌트 작성 ] ///////////
+// 컴포넌트 이므로 반드시 첫글자 대문자!!!
+function IronMan() {
+    return(
+        <React.Fragment>
+            <h2>안녕! 나는 아이언맨이야!</h2>
+            <img src="./images/ab1.jpg" alt="아이언맨" />
+        </React.Fragment>
+    );
+} ///////// IronMan 컴포넌트 함수 /////
+
+// 랜더링 하기 ////
+const root2 = ReactDOM.createRoot(
+    document.getElementById("root2"));
+root2.render(<IronMan />);
+
+/*************************************************** 
+    [ Props 사용하기 ]
+    props는 properties 에서 나온말
+    속성들... 즉, 변수에 값을 할당하여 전달하는 방법
+    함수의 전달값과 같고 속성으로 컴포넌트에 보낸다!
+    -> props는 05번 다음번에 자세히 다룬다!
+***************************************************/
+// 내가 좋아하는 색 표시하기!
+function Favorite(props){ // props는 속성셋팅 변수집합
+    return <h2>
+        내가 좋아하는 색은 {props.color}이야!<br />
+        그리고 좋아하는 음식은 {props.food}야!<br />
+        취미는 {props.hobby}이야! 알겠니???
+        </h2>;
+    // 표현식에 {props.color} 는 호출시 보낸 속성명의 값이다!
+} ///////////// Favorite 컴포넌트 함수 ////////////
+
+// 좋아하는 색을 props로 전달할 수 있다!
+const root3 = ReactDOM.createRoot(
+    document.getElementById("root3"));
+// 랜더링시 속성셋팅 형식으로 함수컴포넌트에 값을 전달함!
+root3.render(<Favorite color="빨간색" food="피자" hobby="게임" />);
+// 함수 컴포넌트에서는 표현식안에서 {props.호출시사용한속성명}
+// 여기서는 {props.color}를 사용한다!
