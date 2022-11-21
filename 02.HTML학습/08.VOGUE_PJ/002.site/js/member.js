414d2379ea277dec0e8bb3626f4014fd0456cc6a
Tom B. Parker <tombap8@hotmail.com>
2022-11-21 10:45:50 +0900

    Update member.js

--- 02.HTML학습/08.VOGUE_PJ/002.site/js/member.js
+++ 02.HTML학습/08.VOGUE_PJ/002.site/js/member.js
@@ -11,10 +11,18 @@ $(()=>{
         - 이벤트종류 : blur(포커스가 빠질때 발생)
         - 제이쿼리 이벤트 메서드 : blur()
         - 이벤트 대상:
+        -> id가 'email2'가 아니고 class가 'search'가 
+        아닌 type이 'text'인 입력요소 input
+        과 함께 type이 'password'인 입력요소 input
 
-
+        ->>>> 제이쿼리 선택표현법:
+        input[type=text][id!=email2][class!=search],
+        input[type=password]
+        >>> 대괄호는 속성선택자, != 같지않다(제이쿼리전용)
+        
     *********************************************/
-   $("input").blur(function(){
+   $(`input[type=text][id!=email2][class!=search],
+   input[type=password]`).blur(function(){
         console.log("블러발생!");
    }); //////////// blur //////////////
 
