512db06ded84d02e3f8d97393315b1d08d2b5bcd
Tom B. Parker <tombap8@hotmail.com>
2022-11-24 16:05:03 +0900

    sss

--- 02.HTML학습/08.VOGUE_PJ/002.site/js/common.js
+++ 02.HTML학습/08.VOGUE_PJ/002.site/js/common.js
@@ -1,12 +1,25 @@
 // 보그PJ 공통JS - common.js
 
+// 윈도우 가로 크기가 모바일 사이즈면 코드를 변경한다!
+let mobsts = 0;
+if ($(window).width() <= 500) mobsts = 1;
+console.log("모바일적용여부:", mobsts);
+
+// 이건 순전히 개발자들 브라우저 크기변경 테스트때문에 구현함
+$(window).resize(() => {
+    // 브라우저 화면크기변경시 계속발생!
+    if ($(window).width() <= 500) mobsts = 1;
+    else mobsts = 0;
+    console.log("모바일적용여부(resize):", mobsts);
+}); /////// resize //////////////
+
 // [현재 페이지명을 알아내어 제어에 활용한다!]
 // 페이지명 변수
 let pname = location.pathname;
 // location.pathname 페이지명이 포함된 전체경로
 // split(자를문자열) -> 배열에 담긴다!
-pname = pname.split("/");//배열에 담는다
-pname = pname[pname.length-1];// 마지막 배열
+pname = pname.split("/"); //배열에 담는다
+pname = pname[pname.length - 1]; // 마지막 배열
 pname = pname.split(".")[0]; // 페이지이름만 가져옴
 // console.log("페이지이름:",pname);
 
@@ -68,15 +81,10 @@ $(() => {
     ///////////////////////////////////////
     //////// 스크롤 이벤트 함수 /////////////
     $(window).scroll(() => {
-
         // 슬림메뉴와 상단이동버튼 보이기 작동안할 페이지셋팅
-        if(
-            pname === "login" ||
-            pname === "member" ||
-            pname === "gallery") {
+        if (pname === "login" || pname === "member" || pname === "gallery") {
             return; // 여기서 나감!
         } ////////// if /////////////
-
 
         // 스크롤 위치값(this는 window)
         scTop = $(this).scrollTop();
@@ -140,7 +148,7 @@ $(() => {
         // 3. 등장액션 적용하기 ///////
         // 스크롤 등장액션 검사함수 호출!
         // 등장요소 개수만큼 자동으로 돌아주면 호출!
-        scAct.each(idx=>scAction(idx));
+        scAct.each((idx) => scAction(idx));
     }); //////// scroll /////////////////
 
     /**************************************** 
@@ -181,6 +189,5 @@ $(() => {
         // 위치이동후 부드러운 스크롤위치로 다시이동됨!
         // pos 전역변수에 값넣기
         pos = 0; // 최상단 위치인 0을 넣는다!
-
     }); /////// click ///////////
 }); //////////////// jQB ///////////////////
