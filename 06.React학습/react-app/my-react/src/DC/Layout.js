877fddb9be617d73bd9f373524d07d590bc5ae06
Tom B. Parker <tombap8@hotmail.com>
2022-11-30 11:47:33 +0900

    Update Layout.js

--- 06.React학습/react-app/my-react/src/DC/Layout.js
+++ 06.React학습/react-app/my-react/src/DC/Layout.js
@@ -1,10 +1,23 @@
 import {Outlet, Link} from "react-router-dom";
+import "./Layout.css";
+/* 
+    [ 리엑트 스타일링 기법 3가지 ]
+    1. 일반 CSS파일을 컴포넌트 JS파일에 import하여 사용
+    2. 객체를 만들어서 적용하는 방법
+    예)
+    const mystyle = {color:"red",fontSize:"30px"};
+    <h1 style={mystyle}>하하하</h1>
+    3. 직접 중괄호 표현식에 중괄호를 하나 더 하여 표현
+    -> 중괄호 하나는 표현식, 안에 중괄호는 객체(CSS용)
+    예)
+    <h1 style={{color:"red"}}>하하하</h1>
+*/
 
 const Layout = () => {
     return(
         <>
         {/* 네비게이션 파트 */}
-            <nav>
+            <nav className="gnb">
                 <ul>
                     <li>
                         로고
