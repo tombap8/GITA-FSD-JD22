e1f8a6a2b4968378d3115f05ebc5f6f38ae6850f
Tom B. Parker <tombap8@hotmail.com>
2023-03-14 17:31:54 +0900

    Update calendar.js

--- 998.기능테스트/JS달력/calendar.js
+++ 998.기능테스트/JS달력/calendar.js
@@ -19,17 +19,43 @@ let currentMonth = currentDate.getMonth();
 let monthArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 let currentYear = currentDate.getFullYear();
 
-
 // 두자릿수 표시함수
-const twoDigit = x => x<10?"0"+x:x;
+const twoDigit = (x) => (x < 10 ? "0" + x : x);
+// 이동버튼 클릭상태값(1-버튼클릭/0-아님)
+let mvBtnSts = 0;
 
 // Calendar 함수
 function Calendar() {
+    let firstData;
+    let secondData;
+    console.log("temp:",temp," | 버튼클릭상태:",mvBtnSts," | 현재월:",currentMonth);
     // 월, 년도 표시
-    showMonth.textContent = `${monthArray[currentMonth]}`;
-    showYear.textContent = `${currentYear}`;
+    if ((temp === "" && !mvBtnSts) || mvBtnSts) {
+        console.log("첫번째조건");
+        firstData = `${monthArray[currentMonth]}`;
+        secondData = `${currentYear}`;
+    } else {
+      console.log("두번째조건");
+        firstData = `${monthArray[parseInt(temp.split("-")[1])-1]}`;
+        secondData = `${temp.split("-")[0]}`;
+        // 해당월로 업데이트!
+        currentMonth = parseInt(temp.split("-")[1])-1;
+        // 해당년도 업데이트!
+        currentYear = temp.split("-")[0];
+    }
+
+
+
+    showMonth.textContent = firstData;
+    showYear.textContent = secondData;
 
     showDate.innerHTML = "";
+
+
+    
+    console.log("현재년:",currentYear);
+    console.log("현재월:",currentMonth);
+
 
     // 주 표시
     for (let i = 1; i <= 7; i++) {
@@ -56,7 +82,7 @@ function Calendar() {
         const span = create("span");
         span.textContent = prevMonthLastDay.getDate() - firstDay.getDay() + i;
         cell.appendChild(span);
-        span.setAttribute("data-d",`${firstDay.getFullYear()}-${twoDigit(firstDay.getMonth()+1)}-${twoDigit(i)}`)
+        span.setAttribute("data-d", `${firstDay.getFullYear()}-${twoDigit(firstDay.getMonth())}-${twoDigit(span.textContent)}`);
     }
 
     // 해당 월 날짜 넣기
@@ -65,12 +91,12 @@ function Calendar() {
         cell.classList.add("grid-cell", "date");
         showDate.appendChild(cell);
 
-        console.log("요기", temp);
+        // console.log("요기", temp);
 
         const span = create("span");
         span.textContent = i;
         cell.appendChild(span);
-        span.setAttribute("data-d",`${lastDay.getFullYear()}-${twoDigit(lastDay.getMonth()+1)}-${twoDigit(i)}`)
+        span.setAttribute("data-d", `${lastDay.getFullYear()}-${twoDigit(lastDay.getMonth() + 1)}-${twoDigit(i)}`);
 
         if (i === currentDate.getDate() && currentMonth === currentDate.getMonth() && currentYear === currentDate.getFullYear()) {
             cell.classList.add("today");
@@ -89,7 +115,7 @@ function Calendar() {
             const span = create("span");
             span.textContent = i;
             cell.appendChild(span);
-            span.setAttribute("data-d",`${lastDay.getFullYear()}-${twoDigit(lastDay.getMonth()+2)}-${twoDigit(i)}`)
+            span.setAttribute("data-d", `${lastDay.getFullYear()}-${twoDigit(lastDay.getMonth() + 2)}-${twoDigit(i)}`);
         }
     }
 }
@@ -108,6 +134,8 @@ function clickFn() {
             currentYear--;
         }
         console.log(currentMonth);
+        // 이동버튼 상태값 변경
+        mvBtnSts = 1;
         Calendar();
         clickFn();
         if (temp !== "") chkDate(temp);
@@ -120,26 +148,33 @@ function clickFn() {
             currentYear++;
         }
         console.log(currentMonth);
+        // 이동버튼 상태값 변경
+        mvBtnSts = 1;
         Calendar();
         clickFn();
         if (temp !== "") chkDate(temp);
     };
 
     // 클릭한 경우 날짜표시 셋팅체크함수
-    const chkDate = d => {
-            let nowTg = showDate.querySelectorAll(".date");
+    const chkDate = (d) => {
+        let nowTg = showDate.querySelectorAll(".date");
 
-            for (let x of nowTg) {
-                let dtg = x.querySelector("span").getAttribute("data-d");
-            console.log("이거야!", d, dtg);
-                if (dtg === d) x.classList.add("on");
-                else x.classList.remove("on");
-            }
-
+        for (let x of nowTg) {
+            let dtg = x.querySelector("span").getAttribute("data-d");
+            // console.log("이거야!", d, dtg);
+            if (dtg === d) x.classList.add("on");
+            else x.classList.remove("on");
+        }
     }; ///////// chkDate함수 //////////////////
 
     // datepicker을 클릭할 때
     datepicker.onclick = () => {
+      
+      // 이동버튼 상태값 변경
+      mvBtnSts = 0;
+      Calendar();
+      clickFn();
+
         if (temp !== "") chkDate(temp);
         calendarBg.classList.add("block");
         calendar.classList.add("block");
@@ -147,6 +182,11 @@ function clickFn() {
 
     // calendarBg를 클릭할 때
     calendarBg.onclick = () => {
+
+      // 바깥쪽 클릭시 년,월 초기화!
+      currentMonth = currentDate.getMonth();
+      currentYear = currentDate.getFullYear();
+      
         calendarBg.classList.remove("block");
         calendar.classList.remove("block");
     };
@@ -154,21 +194,25 @@ function clickFn() {
     // 날짜를 클릭할 때
     let dates = qsa(".calendar-grid .grid-cell.date");
     dates.forEach(function (date) {
-        console.log(333);
-        date.addEventListener("click", function () {
-            let year = qs(".calendar-nav .contents_con .date .year").textContent;
-            let month = monthArray.indexOf(qs(".calendar-nav .contents_con .date .month").textContent) + 1;
-            let clickedDate = this.textContent;
+        // console.log(333);
+        date.onclick = function () {
+            // let year = qs(".calendar-nav .contents_con .date .year").textContent;
+            // let month = monthArray.indexOf(qs(".calendar-nav .contents_con .date .month").textContent) + 1;
+            // let clickedDate = this.textContent;
 
-            if (month < 10) {
-                month = "0" + month;
-            }
+            // if (month < 10) {
+            //     month = "0" + month;
+            // }
 
-            if (clickedDate < 10) {
-                clickedDate = "0" + clickedDate;
-            }
+            // if (clickedDate < 10) {
+            //     clickedDate = "0" + clickedDate;
+            // }
+            // console.log(this);
+            
+            // 이동버튼 상태값 변경
+            mvBtnSts = 0;
 
-            datepicker.value = year + "-" + month + "-" + clickedDate;
+            datepicker.value = this.querySelector("span").getAttribute("data-d");
             calendarBg.classList.remove("block");
             calendar.classList.remove("block");
 
@@ -176,8 +220,8 @@ function clickFn() {
 
             //   Calendar();
 
-            // console.log(temp);
-        });
+            console.log("달력날짜클릭:",temp);
+        };
     });
 } /////////// clickFn 함수 ///////////////////////////
 
