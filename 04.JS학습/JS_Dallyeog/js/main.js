62fe03c1eb566392f8d859851ee069263b8a23e9
Tom B. Parker <tombap8@hotmail.com>
2023-03-26 23:10:01 +0900

    ss

--- 04.JS학습/JS_Dallyeog/js/main.js
+++ 04.JS학습/JS_Dallyeog/js/main.js
@@ -1,1 +1,21 @@
 // 2023캘린더
+
+// 가져오기
+import MakeDallyeog from "./calendar.js";
+
+
+// 생성자함수를 인스턴스로 호출하기
+// MakeDallyeok(요소선택자)
+// -> .calbx
+let calbx = new MakeDallyeog(".calbx");
+calbx.initDallyeok();
+
+let calbx2 = new MakeDallyeog(".calbx2");
+calbx2.initDallyeok();
+
+$(".myipt").click(function(){
+    console.log(this);
+    $(this).next().find(".calender").show();
+})
+
+
