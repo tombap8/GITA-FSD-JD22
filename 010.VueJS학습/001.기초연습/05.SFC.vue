ca4045770a7aa69ad8b5cc46c01f29a51a5c323a
Tom B. Parker <tombap8@hotmail.com>
2023-04-30 18:20:49 +0900

    ss

---
+++ 010.VueJS학습/001.기초연습/05.SFC.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="mytemp">
+        <span class="tit">{{ text }}</span>
+    </div>
+</template>
+
+<script>
+export default{
+    name: "Mytemp",
+    data(){
+        return {
+            text: "연습이예용~~!"
+        }
+    }
+}
+</script>
+
+<style scoped>
+    span.tit {color: red;}
+</style>
