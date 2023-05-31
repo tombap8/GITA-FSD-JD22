79a8c93fd35046889dbbb3f54d0bafe629994037
Tom B. Parker <tombap8@hotmail.com>
2023-05-31 23:24:51 +0900

    ss

--- 06.React학습/react-app/my-react/src/DC/CHARACTERS.js
+++ 06.React학습/react-app/my-react/src/DC/CHARACTERS.js
@@ -1,12 +1,20 @@
 // DC 캐릭터 페이지 JS
 import React from "react";
 import isrc from "./ImgSrc";
+import $ from "jquery";
+
 
 const CHARACTERS = () => {
+
+    const chg = () =>{
+        console.log("하하하");
+        $("#myimg").animate({marginLeft:"200px"},1000)
+    }
+
     return(
         <>
             <h2>CHARACTERS 페이지</h2>
-            <img src={isrc.character} />
+            <img id="myimg" src={isrc.character} onClick={chg} />
         </>
     );
 };
--- 06.React학습/react-app/my-react/src/DC/ImgSrc.js
+++ 06.React학습/react-app/my-react/src/DC/ImgSrc.js
@@ -2,14 +2,14 @@
 
 const isrc = {
     // 각 서브 페이지 이미지경로
-    character:"https://static.dc.com/2022-11/DC_Home_20221128_GothamKnightsEE.jpg?w=1200",
-    comics:"https://www.dc.com/sites/default/files/Gallery_20221129_Aquamen_637ecbd22c85b6.02777926.jpg",
-    games:"https://www.dc.com/sites/default/files/styles/gallery_image_1400x495/public/Games-Marquee_GothamKnights_633f3929bfe9c8.93942640.jpg?itok=Lm6qz8_I",
-    movies:"https://static.dc.com/2022-11/DC_homepagehero_2.jpg?w=1200",
-    news:"https://www.dc.com/sites/default/files/Marquee_BatmanSpawn_635c767f3680f6.49550921.jpg",
+    character:"./images/dcm23.jpg",
+    comics:"./images/dcm24.jpg",
+    games:"./images/dcm26.jpg",
+    movies:"./images/dcm25.jpg",
+    news:"./images/dcm29.jfif",
     video:"https://www.youtube-nocookie.com/embed/fXEOXVZIQVM?wmode=opaque&controls=&modestbranding=1&rel=0&showinfo=0&color=white&autohide=1&enablejsapi=1&origin=https%3A%2F%2Fwww.dc.com",
     // 로고
-    logo: "https://www.dc.com/sites/all/themes/dc_comics_bp/logo.png",
+    logo: "./images/dclogo.svg",
     // 에러페이지
     error:"https://shop.dc.com/coming-soon/static/media/mxyzptlk.0f810a8a.png"
 };
