2d6774228764258f7cdf3f073194054803312bc3
Tom B. Parker <tombap8@hotmail.com>
2023-06-01 23:42:47 +0900

    ss

--- 06.React학습/react-app/tom-app/src/dc/Ban.js
+++ 06.React학습/react-app/tom-app/src/dc/Ban.js
@@ -3,12 +3,45 @@
 import "./css/ban.css";
 // 배너 데이터
 import ban_data from "./data/banner";
+import $ from "jquery";
+
+$(()=>{
+
+
+    let prot = 0;
+    $(".abtn").click(function(){
+        if(prot) return;
+        prot=1;
+        setTimeout(()=>prot=0,400);
+
+        let isB = $(this).is(".rb");
+        console.log(isB);
+
+        let tg = $(this).siblings(".slider");
+
+        if(isB){
+            tg.animate({left:"-100%"},400,
+            function(){
+                $(this).append($(this).find("li").first()).css({left:"0"});
+            })
+        }
+        else{
+            tg
+            .prepend(tg.find("li").last()).css({left:"-100%"})
+            .animate({left:"0"},400);
+        }
+
+
+        $(".indic li").eq(tg.find("li").eq(isB).attr("data-seq")).addClass("on")
+        .siblings().removeClass("on");
+    })
+});
 
 // 반복리스트 코드 생성용 컴포넌트 ///////
 function MakeList(props) { 
     // rec - 개별레코드값(객체형식)
     return (
-        <li>
+        <li data-seq={props.idx}>
             <img className="banimg" src={props.rec["src"]} alt="배너" />
             <section className="bantit">
                 <h3>{props.rec["tit1"]}</h3>
@@ -31,9 +64,22 @@ function Ban(props) {
             <ul className="slider">
                 {
                     sel_data.map((x,i)=> 
-                    <MakeList rec={x} key={i} />)
+                    <MakeList rec={x} key={i} idx={i} />)
                 }
             </ul>
+            {
+                sel_data.length != 1 &&
+                <>
+                    <button className="abtn lb">＜</button>
+                    <button className="abtn rb">＞</button>
+                    <ol className="indic">
+                        {
+                            sel_data.map((x,i)=>
+                            <li className={i==0?'on':''} key={i}></li>)
+                        }
+                    </ol>
+                </>
+            }
         </div>
     );
 } /////////// Ban 컴포넌트 /////////////
