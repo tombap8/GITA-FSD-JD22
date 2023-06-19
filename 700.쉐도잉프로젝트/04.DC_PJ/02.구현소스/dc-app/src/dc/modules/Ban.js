454ae439d62f1c6bd7a2d52f587ed84595f7985d
Tom B. Parker <tombap8@hotmail.com>
2023-06-20 00:05:56 +0900

    ss

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/Ban.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/modules/Ban.js
@@ -44,46 +44,44 @@ function jqFn(obj) {
         let prot = 0;
 
         // 1. 버튼 클릭시 이동기능구현
-        $(this).find(".abtn").on("click",function () {
-            // 0.광클금지
-            if (prot) return;
-            prot = 1;
-            setTimeout(() => (prot = 0), 400);
+        $(obj)
+            .find(".abtn")
+            .on("click", function () {
+                // 0.광클금지
+                if (prot) return;
+                prot = 1;
+                setTimeout(() => (prot = 0), 400);
 
-            // 1. 버튼구분하기
-            let isB = $(this).is(".rb");
-            console.log("오른쪽?", isB);
+                // 1. 버튼구분하기
+                let isB = $(this).is(".rb");
+                console.log("오른쪽?", isB);
 
-            // 슬라이드 타겟설정 : 클릭된 버튼의 형제요소 슬라이더
-            const tg = $(this).siblings(".slider");
+                // 슬라이드 타겟설정 : 클릭된 버튼의 형제요소 슬라이더
+                const tg = $(this).siblings(".slider");
 
-            // 2. 분기하여 기능구현하기
-            // (1) 오른쪽버튼 클릭시 : 오른쪽에서 들어옴(left:0->-100%)
-            if (isB) {
-                tg.animate({ left: "-100%" }, 400, function () {
-                    // this는 타겟!
-                    // 첫번째 li 맨뒤로 보내기! 동시에 left:0
-                    $(this).append($(this).find("li").first()).css({ left: "0" });
-                }); /////// animate ///////
-            } ///////// if ////////////////
+                // 2. 분기하여 기능구현하기
+                // (1) 오른쪽버튼 클릭시 : 오른쪽에서 들어옴(left:0->-100%)
+                if (isB) {
+                    tg.animate({ left: "-100%" }, 400, function () {
+                        // this는 타겟!
+                        // 첫번째 li 맨뒤로 보내기! 동시에 left:0
+                        $(this).append($(this).find("li").first()).css({ left: "0" });
+                    }); /////// animate ///////
+                } ///////// if ////////////////
 
-            // (2) 왼쪽버튼 클릭시 : 왼쪽에서 들어옴(left:-100%->0)
-            else {
-                // 마지막 li 맨앞이동+동시에 left:-100% 후 left:0 애니
-                tg.prepend(tg.find("li").last()).css({ left: "-100%" }).animate({ left: "0" }, 400);
-            } //////////// else ////////////////
+                // (2) 왼쪽버튼 클릭시 : 왼쪽에서 들어옴(left:-100%->0)
+                else {
+                    // 마지막 li 맨앞이동+동시에 left:-100% 후 left:0 애니
+                    tg.prepend(tg.find("li").last()).css({ left: "-100%" }).animate({ left: "0" }, 400);
+                } //////////// else ////////////////
 
-            // 3. 배너와 일치하는 블릿에 클래스 "on"넣기(나머지는 "on"제거)
-            // 대상: .indic li
-            // eq(순번) -> 오른쪽이동시 1, 왼쪽이동시 0
-            // isB값으로 오른쪽은 true->1, 왼쪽은 false->0
-            // 순서가 바뀌는 슬라이드에 고유 순번속성 data-seq값을 읽어옴!
-            $(this).find(".indic li")
-                .eq(tg.find("li").eq(isB).attr("data-seq"))
-                .addClass("on")
-                .siblings()
-                .removeClass("on");
-        }); ////////// click ////////////
+                // 3. 배너와 일치하는 블릿에 클래스 "on"넣기(나머지는 "on"제거)
+                // 대상: .indic li
+                // eq(순번) -> 오른쪽이동시 1, 왼쪽이동시 0
+                // isB값으로 오른쪽은 true->1, 왼쪽은 false->0
+                // 순서가 바뀌는 슬라이드에 고유 순번속성 data-seq값을 읽어옴!
+                $(obj).find(".indic li").eq(tg.find("li").eq(isB).attr("data-seq")).addClass("on").siblings().removeClass("on");
+            }); ////////// click ////////////
         console.log("배너jqFn");
     }); ///////////// jQB ////////////////
 } ///////////// jqFn 함수 /////////////////
@@ -109,12 +107,52 @@ function Ban(props) {
     // props.cat 은 배너데이터 구분속성명
     const sel_data = ban_data[props.cat];
     // sel_data에 담긴값은 data/banner.js의 객체의 배열값
-    const loadFn = () => {
+    const loadFn = function () {
         console.log(this);
         jqFn(this);
-    }
+    };
+    // 광클금지변수
+    let prot = 0;
+
+    const goSlide = (e) => {
+        // console.log(e.target)
+        // 0.광클금지
+        if (prot) return;
+        prot = 1;
+        setTimeout(() => (prot = 0), 400);
+
+        // 1. 버튼구분하기
+        let isB = $(e.target).is(".rb");
+        console.log("오른쪽?", isB);
+
+        // 슬라이드 타겟설정 : 클릭된 버튼의 형제요소 슬라이더
+        const tg = $(e.target).siblings(".slider");
+
+        // 2. 분기하여 기능구현하기
+        // (1) 오른쪽버튼 클릭시 : 오른쪽에서 들어옴(left:0->-100%)
+        if (isB) {
+            tg.animate({ left: "-100%" }, 400, function () {
+                // this는 타겟!
+                // 첫번째 li 맨뒤로 보내기! 동시에 left:0
+                $(this).append($(this).find("li").first()).css({ left: "0" });
+            }); /////// animate ///////
+        } ///////// if ////////////////
+
+        // (2) 왼쪽버튼 클릭시 : 왼쪽에서 들어옴(left:-100%->0)
+        else {
+            // 마지막 li 맨앞이동+동시에 left:-100% 후 left:0 애니
+            tg.prepend(tg.find("li").last()).css({ left: "-100%" }).animate({ left: "0" }, 400);
+        } //////////// else ////////////////
+
+        // 3. 배너와 일치하는 블릿에 클래스 "on"넣기(나머지는 "on"제거)
+        // 대상: .indic li
+        // eq(순번) -> 오른쪽이동시 1, 왼쪽이동시 0
+        // isB값으로 오른쪽은 true->1, 왼쪽은 false->0
+        // 순서가 바뀌는 슬라이드에 고유 순번속성 data-seq값을 읽어옴!
+        $(e.target).parents(".banner").find(".indic li").eq(tg.find("li").eq(isB).attr("data-seq")).addClass("on").siblings().removeClass("on");
+    };
     return (
-        <div className="banner" onLoad={loadFn}>
+        <div className="banner" onLoad={() => loadFn(this)}>
             {/* 이동 슬라이드 */}
             <ul className="slider">
                 {sel_data.map((x, i) => (
@@ -127,8 +165,12 @@ function Ban(props) {
                 sel_data.length > 1 && (
                     <>
                         {/* 양쪽이동버튼 */}
-                        <button className="abtn lb">＜</button>
-                        <button className="abtn rb">＞</button>
+                        <button className="abtn lb" onClick={goSlide}>
+                            ＜
+                        </button>
+                        <button className="abtn rb" onClick={goSlide}>
+                            ＞
+                        </button>
                         {/* 블릿 인디케이터 */}
                         <ol className="indic">
                             {sel_data.map((x, i) => (
